d42c64a1c078a2be4f04ee1d1fd4ddaffea7f350
SUMB29 <basaksumit708@gmail.com>
2025-12-11 22:00:33 +0530

    title added

--- payment.php
+++ payment.php
@@ -71,7 +71,7 @@ if (isset($_POST['confirm_payment'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Payment | Movie Booking</title>
+    <title>CineBook | Payment | Movie Booking</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
