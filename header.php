fb9998bc4c998341bd951f3517cd69b667e5b608
SUMB29 <basaksumit708@gmail.com>
2025-12-11 10:30:55 +0530

    header updated

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie Ticket Booking</title>
+    <title>CineBook-header</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-black">
